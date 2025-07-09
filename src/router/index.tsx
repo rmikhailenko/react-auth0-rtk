4b014c23a8f59a64d037ebb37aad9bce8a2d75a9
Roman Mikhailenko <mikhoilenko.roman@gmail.com>
2025-07-09 23:01:05 +0300

    add react router, add root layout, implement routing, init header

---
+++ src/router/index.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layouts/RootLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [{}],
+  },
+]);
