23acf41e25809cd8ec290a9903cfc86013213c45
Roman Mikhailenko <mikhoilenko.roman@gmail.com>
2025-07-09 22:01:58 +0300

    add auth 0, update vite host config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "localhost", // Use 'localhost' to bind to the local network interface
+    port: 3000,
+  },
+});
