9414433eb518559a7f71ebacd6f2ce632a03af36
Roman Mikhailenko <mikhoilenko.roman@gmail.com>
2025-07-09 22:39:57 +0300

    remove default styles, add auth provider, add env types

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,19 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  );
+}
